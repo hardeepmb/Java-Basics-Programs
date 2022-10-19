738e114bde24c7670611f4918cdb7338cb23254b
mjolnir31 <85400775+mjolnir31@users.noreply.github.com>
2022-10-19 23:08:20 +0530

    add a new prg

--- src/com/tutorial/resstr11recus1.java
+++ src/com/tutorial/resstr11recus1.java
@@ -5,9 +5,14 @@ import java.util.*;
 public class resstr11recus1 {
     public static void main(String[] args) {
 
+        String s = "Hardeep";
+        System.out.println(res(s));
     }
 
-    static String res(String n, int n, String rev) {
-
+    static String res(String s) {
+        if (s == null || s.length() <= 1) {
+            return s;
+        }
+        return res(s.substring(1))+s.charAt(0);
     }
 }
