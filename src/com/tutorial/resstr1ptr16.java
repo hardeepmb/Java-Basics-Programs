7fea014dc61951703a7408e2ca448e3b57f43b2f
mjolnir31 <85400775+mjolnir31@users.noreply.github.com>
2022-10-25 18:05:58 +0530

    star pattern pgm1

---
+++ src/com/tutorial/resstr1ptr16.java
@@ -0,0 +1,16 @@
+package com.tutorial;
+
+public class resstr1ptr16 {
+    public static void main(String[] args) {
+        //    string pattern program 1
+//        Scanner s= new Scanner(System.in);
+        int i,j;
+        for(i =1;i<=4;i++){
+            for (j=4;j>=i;j--){
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
+
---
+++ src/com/tutorial/str1ptr16.java
@@ -0,0 +1,16 @@
+package com.tutorial;
+import java.util.*;
+
+public class str1ptr16 {
+    public static void main(String[] args) {
+        //    string pattern program 1
+        Scanner s= new Scanner(System.in);
+        int i,j;
+        for(i =1;i<=4;i++){
+            for (j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
