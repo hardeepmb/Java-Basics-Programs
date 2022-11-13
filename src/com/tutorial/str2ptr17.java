2572274e9d2861e6eb084d5a6478db7b521aa257
mjolnir31 <85400775+mjolnir31@users.noreply.github.com>
2022-11-13 19:19:56 +0530

    star pattern2 pgm17

---
+++ src/com/tutorial/str2ptr17.java
@@ -0,0 +1,26 @@
+package com.tutorial;
+
+import java.util.Scanner;
+
+public class str2ptr17 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int i, j;
+        for (i=1; i<=4;i++){
+            for (j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+        for(i =1;i<=4;i++){
+            for (j=3;j>=i;j--){
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+
+
+    }
+
+}
+
